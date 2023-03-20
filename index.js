e474aa4b6485b9e98de47bae94f14ca3045d27d7
grandsmarquis <jdourlens@gmail.com>
2023-03-20 18:14:03 +0900

    ban works

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ function getGroups() {
 }
 
 bot.on('message', async (ctx) => {
-
+    console.log(ctx.update)
     // check if it's a new member. If yes we check if he is allowed to be in the group or not
-    if (true) {
+    if (ctx.update.message.new_chat_member != null && ctx.update.message.chat.type == "group") {
+        const userId = ctx.update.message.new_chat_member.id;
+        const chatId = ctx.update.message.chat.id;
         // check if we control this group
         console.log(userId, "joined the chat", chatId)
         // web3 call here
         // ban if not whitelisted
-        if (false) {
+        if (true) {
             console.log("We ban him", userId, chatId)
             ctx.banChatMember(userId, 0, {
                 chat_id: chatId,
@@ -26,7 +28,6 @@ bot.on('message', async (ctx) => {
             })
         }
     }
-    console.log(ctx)
 })
 
 bot.startPolling()
