c9c082f2d50e3267b7c84973458a13b7a46c20a7
grandsmarquis <jdourlens@gmail.com>
2023-03-20 14:53:16 +0900

    save

--- index.js
+++ index.js
@@ -11,9 +11,20 @@ function getGroups() {
 }
 
 bot.on('message', async (ctx) => {
-    // check if it's a new member. If yes we check if he is allowed to be in the group
-    if (true) {
 
+    // check if it's a new member. If yes we check if he is allowed to be in the group or not
+    if (true) {
+        // check if we control this group
+        console.log(userId, "joined the chat", chatId)
+        // web3 call here
+        // ban if not whitelisted
+        if (false) {
+            console.log("We ban him", userId, chatId)
+            ctx.banChatMember(userId, 0, {
+                chat_id: chatId,
+                revoke_messages: true
+            })
+        }
     }
     console.log(ctx)
 })
