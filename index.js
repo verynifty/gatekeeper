1ec4127b68b28ce021218af8111136b87ffed8e1
grandsmarquis <jdourlens@gmail.com>
2023-03-20 13:42:52 +0900

    add dependencies

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf("6153080757:AAH-FYsprOXth86Is4I-pbB1Gi7o4eLIEDY");
+
+function getGroups() {
+    return ([
+        id: "chatId"
+    ])
+}
+
+while (true) {
+    let groups = getGroups();
+}
