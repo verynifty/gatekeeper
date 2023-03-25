531b0f7889da18a402ed2bc4129e832702370001
grandsmarquis <jdourlens@gmail.com>
2023-03-25 12:12:09 +0900

    typo

--- api/[id].js
+++ api/[id].js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     res.statusCode = 200;
-    const id = request.query.id;
+    const id = req.query.id;
 
     res.setHeader('Content-Type', 'application/json');
     res.json({ name: 'John Doe', id: id });
