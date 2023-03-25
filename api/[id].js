79020b61796aa522107f909e058cf1f9ab8b3516
grandsmarquis <jdourlens@gmail.com>
2023-03-25 12:08:08 +0900

    route

--- api/[id].js
+++ api/[id].js
@@ -1,5 +1,7 @@
 export default function handler(req, res) {
     res.statusCode = 200;
+    const id = request.query.id;
+
     res.setHeader('Content-Type', 'application/json');
-    res.json({ name: 'John Doe' });
+    res.json({ name: 'John Doe', id: id });
   }
