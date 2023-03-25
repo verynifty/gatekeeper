40153750537fea7db18f70e5e666a317fc05414d
grandsmarquis <jdourlens@gmail.com>
2023-03-25 12:02:23 +0900

    app

--- api.js
+++ api.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const app = require('express')();
 
 //create a get route with an id as parameter
-router.get('/:id', (req, res) => {
+app.get('/:id', (req, res) => {
     const { id } = req.params;
     // do something with the id
     res.json({id: id});
 };
+
+module.exports = app;
