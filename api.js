7bb2edde0325a67fd24ed6878aa739ac643459a7
grandsmarquis <jdourlens@gmail.com>
2023-03-25 12:41:19 +0900

    get chat

--- api.js
+++ api.js
@@ -17,10 +17,12 @@ GK_ABI = require('../GATEKEEPER_ABI.json');
 const GK = new ethers.Contract(GK_ADDRESS, GK_ABI, web3);
 
 //create a get route with an id as parameter
-app.get('/:id', (req, res) => {
+app.get('/:id', async (req, res) => {
     const { id } = req.params;
     // do something with the id
-    res.json({id: id});
+    let chatId = await GK.roomIds(id);
+    let chatInfo = await bot.telegram.getChat(chatId.toString())
+    res.json(chatInfo);
 };
 
 module.exports = app;
